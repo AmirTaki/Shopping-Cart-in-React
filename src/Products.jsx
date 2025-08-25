afca0ee81df614e4a8fe00fd5668daa18cdcc5a8
amir <amirtaki623@gmail.com>
2025-08-25 23:01:34 +0330

    part 29

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,10 +1,10 @@
-
+import imageProduct1 from "./assets/images/1.png"
 
 export const Products = [
     {
         id : 1,
         name : "",
         price : 101, 
-        
+        image : imageProduct1,
     }
 ] 
