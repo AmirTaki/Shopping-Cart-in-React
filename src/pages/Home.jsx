ab23d08aaa4db49e2158e7af230746da5f3f99c5
amir <amirtaki623@gmail.com>
2025-08-25 23:09:07 +0330

    part 37

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,11 @@ const Home = () => {
     return (
        <div>
             <h1 className="text-3xl my-5">List Products</h1>
-            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5"></div>
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5">
+                {products.map((product, key) => {
+                    <></>
+                })}
+            </div>
        </div>
     )
 }
