aceba8081c71b7af64110bdaa7a64910c3e61198
amir <amirtaki623@gmail.com>
2025-08-25 23:08:24 +0330

    part 36

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import {products} from "../Products"
+
 const Home = () => {
     return (
        <div>
             <h1 className="text-3xl my-5">List Products</h1>
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5"></div>
        </div>
     )
 }
