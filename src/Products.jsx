e58f79857f65acf4189b86380e57126c24bd26f0
amir <amirtaki623@gmail.com>
2025-08-25 23:06:44 +0330

    part 35

--- src/Products.jsx
+++ src/Products.jsx
@@ -48,5 +48,29 @@ export const Products = [
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
         slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
     },
+     {
+        id: 6,
+        name: 'Jaqueze Upholstered Armchair',
+        price: 111,
+        image: imageProduct6,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'corrigan-studio-273-wide-armchair'
+    },
+    {
+        id: 7,
+        name: 'Leston Wide Upholstered Fabric',
+        price: 121,
+        image: imageProduct7,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'leston-wide-upholstered-fabric'
+    },
+    {
+        id: 8,
+        name: 'Stephanny 27.5" Wide Tufted',
+        price: 220,
+        image: imageProduct8,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'stephanny-275-wide-tufted-armchair'
+    },
   
 ] 
