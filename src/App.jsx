3917f8859e12a95a6e671ded3131ae829ee324c2
amir <amirtaki623@gmail.com>
2025-08-25 22:23:31 +0330

    part 11

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,14 @@ import Layout from  "./components/Layout";
 import Header from "./components/Header";
 import CarTab from "./components/CarTab";
 import Home from "./pages/Home";
-import Detail from "./pages/Detail"
+import Detail from "./pages/Detail";
+import {Route, Routes, BrowserRouter} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline text-red-200">
-        Hello world!
-       </h1>
-       <Layout />
-      <Header />
-      <CarTab />
-      <Home />
-      <Detail />
+  
     </>
 
   )
