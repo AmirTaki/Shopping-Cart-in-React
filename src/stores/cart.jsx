a5f5f279c6197a0282cc75d272eb7649079bad0f
amir <amirtaki623@gmail.com>
2025-08-25 23:46:59 +0330

    part 55

--- src/stores/cart.jsx
+++ src/stores/cart.jsx
@@ -8,9 +8,11 @@ const cartSlice = createSlice({
     initialState, 
     reducers: {
         addToCart(state, action){
-            const {productId, quantity}   
+            const {productId, quantity}   = action.payload;
+            state.items.push ({productId, quantity})
         }
     }
 })
 
+export const {addToCart} = cartSlice.actions;
 export default cartSlice.reducer;
