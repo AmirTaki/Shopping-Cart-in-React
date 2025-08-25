d576d26a62d04f7e86291092458e70c98e9d2743
amir <amirtaki623@gmail.com>
2025-08-25 23:28:05 +0330

    part 42

--- src/components/productCart.jsx
+++ src/components/productCart.jsx
@@ -1,8 +1,14 @@
+import { Link, Route } from "react-router-dom";
+
+
 const ProductCart = (props) => {
     const {id, name, price, image, slug} = props.data;
     return(
-        <div>
-            aafd
+        <div className="bg-white p-5 rounded-xl shadow-sm">
+            <Link to = {slug}>
+                <img src={image} alt="" className="w-full h-80 object-cover object-top drop-shadow-[0_80px_30px_#0007]"/>
+
+            </Link>
         </div>
     )
 }
