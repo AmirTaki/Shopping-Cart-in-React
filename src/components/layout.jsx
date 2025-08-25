d07731cda80cb563d7bfbb16f295a19d4743d70c
amir <amirtaki623@gmail.com>
2025-08-25 22:49:35 +0330

    part 20

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,13 +5,13 @@ import CarTab from "./CarTab";
 
 const Layout = () => {
     return(
-        <>
-            <main>
+        <div className="bg-zinc-200">
+            <main className="w-[1200px] max-w-full m-auto p-5">
                 <Header />
                 <Outlet />
             </main>
             <CarTab />
-        </>
+        </div>
     )
 }
 
