54576be77c61cd4ac0a49d38ceea922e75d45d13
amir <amirtaki623@gmail.com>
2025-08-25 23:28:41 +0330

    part 43

--- src/components/productCart.jsx
+++ src/components/productCart.jsx
@@ -7,8 +7,8 @@ const ProductCart = (props) => {
         <div className="bg-white p-5 rounded-xl shadow-sm">
             <Link to = {slug}>
                 <img src={image} alt="" className="w-full h-80 object-cover object-top drop-shadow-[0_80px_30px_#0007]"/>
-
             </Link>
+            <h3 className="text-2xl py-3 text-center font-medium">{name}</h3>
         </div>
     )
 }
