b3c6139eb68d09d7de2bb991453b1ca091aac021
amir <amirtaki623@gmail.com>
2025-08-25 22:30:41 +0330

    part 16

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
     return(
         <>
-            Layout
+            <div className="">Layout <Outlet /></div>
         </>
     )
 }
