442400fcc4799d9636b6e39e5b225e4a685a4625
amir <amirtaki623@gmail.com>
2025-08-25 23:29:53 +0330

    part 44

--- src/components/productCart.jsx
+++ src/components/productCart.jsx
@@ -9,6 +9,11 @@ const ProductCart = (props) => {
                 <img src={image} alt="" className="w-full h-80 object-cover object-top drop-shadow-[0_80px_30px_#0007]"/>
             </Link>
             <h3 className="text-2xl py-3 text-center font-medium">{name}</h3>
+            <div className="flex justify-between items-center">
+                <p>
+                    $<span className="text-2xl font-medium">{price}</span>
+                </p>
+            </div>
         </div>
     )
 }
