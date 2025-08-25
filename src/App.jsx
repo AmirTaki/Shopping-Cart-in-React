f2fe377fe5875e7a982db4fc0fe57c70dbde2370
amir <amirtaki623@gmail.com>
2025-08-25 22:06:22 +0330

    part 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
-import './App.css'
+import './App.css';
+
+import Layout from  "./components/Layout"
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
       <h1 class="text-3xl font-bold underline text-red-200">
         Hello world!
        </h1>
+       <Layout />
+      
 
     </>
   )
