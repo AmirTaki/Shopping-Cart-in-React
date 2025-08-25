b24ad0bb4c7eabbfef86fb969af3a83b5dc5650c
amir <amirtaki623@gmail.com>
2025-08-25 22:52:00 +0330

    part 22

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import iconCart from "../assets/images/iconCart.png"
 
 const Header = () => {
     return (
         <header>
             <Link to = '/' className="text-xl font-semibold">Home.</Link>
+            <div className="">
+                <img src={iconCart} alt="" />
+            </div>
         </header>
     )
 }
