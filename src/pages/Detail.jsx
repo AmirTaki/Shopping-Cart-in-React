48321a8b415d0b3762ad66bf611295cbeff2b2cc
amir <amirtaki623@gmail.com>
2025-08-26 22:01:18 +0330

    part 7

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
-
 import {Products} from "../Products"
+import { useEffect } from "react";
 
 
 const Detail = () => {
+    const {slug} = useParams();
     return (
         <>
             Detail
