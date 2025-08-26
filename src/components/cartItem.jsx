9687fc5d747395b24d85d3892831fd415ab0dc6f
amir <amirtaki623@gmail.com>
2025-08-26 23:33:57 +0330

    part 44

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,7 +1,11 @@
+
 import { useEffect, useState } from "react";
+import { Products } from "../Products";
+
 
 const CartItem = (props) => {
     const {productId, quantity} = props.data;
+    const [detail, setDetail] = useState([]);
     return (
         <>
         </>
