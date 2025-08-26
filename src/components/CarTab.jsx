ba4d1278037c8b47e373b12eb0b17bb839d577c6
amir <amirtaki623@gmail.com>
2025-08-26 23:15:15 +0330

    part 37

--- src/components/CarTab.jsx
+++ src/components/CarTab.jsx
@@ -3,7 +3,9 @@ import { useSelector } from "react-redux";
 const CarTab = () => {
     useSelector(store => store.cart.items)
     return (
-        <div className="fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px]"></div>
+        <div className="fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px]">
+            <h2 className="p-5 text-white text-2xl">Shopping Cart</h2>
+        </div>
     )
 }
 
