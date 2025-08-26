24fe5bd2767abf304dc3dffe62fc732c9613618f
amir <amirtaki623@gmail.com>
2025-08-26 23:26:04 +0330

    part 378

--- src/components/CarTab.jsx
+++ src/components/CarTab.jsx
@@ -5,6 +5,14 @@ const CarTab = () => {
     return (
         <div className="fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px]">
             <h2 className="p-5 text-white text-2xl">Shopping Cart</h2>
+            <div className="">
+                
+            </div>
+            <div className="grid grid-cols-2">
+                <button>CLOSE</button>
+                <button>CHECKOUT</button>
+            </div>
+
         </div>
     )
 }
