311d1c9ab755bb9e52c7677ee8bbe0e5aca4ce25
amir <amirtaki623@gmail.com>
2025-08-26 23:08:55 +0330

    part 33

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import iconCart from "../assets/images/iconCart.png";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-
+// import { toggleStatusTab } from '../stores/cart'
 
 
 const Header = () => {
@@ -10,19 +10,23 @@ const Header = () => {
     const carts = useSelector(store => store.cart.items);
     useEffect(() => {
         let total = 0;
-        carts.forEach((item) => {
-            total += item.quantity;
-            setTotalQuantity(total)
-        });
-    }, [carts])
+        carts.forEach(item => total += item.quantity);
+        setTotalQuantity(total);
+    }, [carts]);
+
+    //  const handleOpenTabCart = () => {
+    //     dispatch(toggleStatusTab());
+    // }
     return (
         <header className="flex justify-between itmes-center mb-5">
             <Link to = '/' className="text-xl font-semibold">Home.</Link>
-            <div className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative">
+            <div className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative" >
                 <img src={iconCart} alt="" className="w-6" />
                 <span 
                     className="absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full  flex justify-center items-center"
-                >{totalQuantity}</span>
+                >
+                    {totalQuantity}
+                </span>
             </div>
         </header>
     )
