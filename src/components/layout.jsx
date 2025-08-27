f903cfca66ef6dbc09bf56e11a3aad9ebe402ac9
amir <amirtaki623@gmail.com>
2025-08-27 21:33:45 +0330

    part 16

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header  from "../components/Header";
 import CarTab from "./CarTab";
+import { useSelector } from "react-redux";
+import { use } from "react";
 
 
 const Layout = () => {
+   const statusTabCart =  useSelector(store => store.cart.statusTab)
     return(
         <div className="bg-zinc-200">
             <main className="w-[1200px] max-w-full m-auto p-5">
