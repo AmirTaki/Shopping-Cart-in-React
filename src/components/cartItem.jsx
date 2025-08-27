4ca3f3de85e2042928c7ae8e20cda02bb4fd9698
amir <amirtaki623@gmail.com>
2025-08-27 21:05:16 +0330

    part 4

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -17,7 +17,14 @@ const CartItem = (props) => {
     const handleMinusQuantity = ( ) => {
         dispatch(changeQuantity({
             productId: productId,
-            quantity : productId - 1;
+            quantity : quantity - 1,
+        }))
+    }
+
+    const handlePlusQuantity = () => {
+        dispatch(changeQuantity({
+            productId: productId,
+            quantity : quantity + 1,
         }))
     }
 
@@ -29,7 +36,7 @@ const CartItem = (props) => {
             <div className="w-20 flex justify-between ">
                 <button className="bg-gray-200 rounded-full w-6 h-6 text-cyan-600" onClick={handleMinusQuantity}>-</button>
                 <span>{quantity}</span>
-                <button className="bg-gray-200 rounded-full w-6 h-6 text-cyan-600">+</button>
+                <button className="bg-gray-200 rounded-full w-6 h-6 text-cyan-600" onClick={handlePlusQuantity}>+</button>
             </div>
        </div>
     )
