9f4ac1ffa981581f2c39f925e0f696668a359bc8
amir <amirtaki623@gmail.com>
2025-08-27 21:21:52 +0330

    part 12

--- src/components/CarTab.jsx
+++ src/components/CarTab.jsx
@@ -7,7 +7,8 @@ const CarTab = () => {
     const statusTab =  useSelector(store => store.cart.statusTab);
     return (
         <div className={`fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px] 
-            transform ${statusTab === false ? "translate-x-full": ""}
+            transform transition-transform duration-500
+            ${statusTab === false ? "translate-x-full": ""}
         `}>
             <h2 className="p-5 text-white text-2xl">Shopping Cart</h2>
             <div className="p-5">
