5e4d78739ab167e55aafdb8b48953241dd17c496
amir <amirtaki623@gmail.com>
2025-08-27 21:34:40 +0330

    part 17

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -9,7 +9,8 @@ const Layout = () => {
    const statusTabCart =  useSelector(store => store.cart.statusTab)
     return(
         <div className="bg-zinc-200">
-            <main className="w-[1200px] max-w-full m-auto p-5">
+            <main className={`w-[1200px] max-w-full m-auto p-5 transform transition-transform duration-500
+                 ${}`}>
                 <Header />
                 <Outlet />
             </main>
