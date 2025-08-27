609d50dcfb95e210bf5d4f0037876f94feb445b3
amir <amirtaki623@gmail.com>
2025-08-27 21:18:00 +0330

    part 10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import iconCart from "../assets/images/iconCart.png";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-// import { toggleStatusTab } from '../stores/cart'
+import { toggleStatusTab } from '../stores/cart'
 
 
 const Header = () => {
     const [totalQuantity, setTotalQuantity] = useState(0);
     const carts = useSelector(store => store.cart.items);
-     const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    
     useEffect(() => {
         let total = 0;
         carts.forEach(item => total += item.quantity);
@@ -21,7 +22,7 @@ const Header = () => {
     return (
         <header className="flex justify-between itmes-center mb-5">
             <Link to = '/' className="text-xl font-semibold">Home.</Link>
-            <div className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative" >
+            <div className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative" onClick={handleOpenTabCart} >
                 <img src={iconCart} alt="" className="w-6" />
                 <span 
                     className="absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full  flex justify-center items-center"
