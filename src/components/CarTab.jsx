04387e0016d92b0d296d17578a26ca46890dfbda
amir <amirtaki623@gmail.com>
2025-08-27 21:22:29 +0330

    part 13

--- src/components/CarTab.jsx
+++ src/components/CarTab.jsx
@@ -1,10 +1,11 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import CartItem from "./cartItem";
 
 
 const CarTab = () => {
     const carts = useSelector(store => store.cart.items);
     const statusTab =  useSelector(store => store.cart.statusTab);
+    const dispath = useDispatch()
     return (
         <div className={`fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px] 
             transform transition-transform duration-500
